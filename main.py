b991150855f3c755c91d7cef7ad5eff2e6c33704
jennifermel <jennifermelelissa8503@gmail.com>
2024-08-02 17:21:10 -0300

    Lista de todos os estador do brasil

---
+++ main.py
@@ -0,0 +1,7 @@
+#lista
+estados = ['Distrito Federal','Goías','Mato Grosso','Mato Grosso do Sul','Alagoas','Bahia','Ceará','Maranhão','Paraíba','Pernambuco','Rio Garande do Norte','Sergipe','Acre','Amapá','Amazonas','Pará','Rondônia','Roraima','Tocantins','Espiríto Santo','Minas Gerais','Rio de Janeiro','São Paulo','Paraná','Rio Grande do Sul','Santa Catarina',]
+
+#saida de dados
+for nome in estados:
+    print(nome)
+
